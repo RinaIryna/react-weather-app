9c1d229930e80cfe67cd45203ba4916574bf90d8
RinaIryna <soulfelis@gmail.com>
2023-03-12 18:31:14 +0200

    Added API -weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="Search">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                className="form-control"
-                autoFocus="on"
-                placeholder="Enter a city"
-              />
-            </div>
-            <div className="col-3 ">
-              <input
-                type="submit"
-                className="btn btn-primary w-100"
-                value="Search"
-              />
-            </div>
-          </div>
-        </form>
-      </div>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ loaded: false });
 
-      <div className="Overview">
-        <h1>Oslo</h1>
-        <ul>
-          <li>Sunday, 15:23</li>
-          <li>Mostly sunny</li>
-        </ul>
-      </div>
+  function showWeather(response) {
+    setWeatherData({
+      loaded: true,
+      date: "Sunday, 15:23",
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
 
-      <div>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="d-flex weather-temperature">
-              <img
-                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAjdJREFUeNrtmsGtgzAMhjtCRmAERsgIHYFjjx2BERiBEToCI+TaG2+DbJBnKj8pD1Eaiv8AwpX+C1Ap/hzHjpNLCOFyZl0UgAJQAApAASgABaAAFIACODEA9C/83AypIrUkRwpv5Pib4dsCNp5cAMgIS3rMGPxJw3/t4QDQoEtSt8LwsTpJEFAAAONjNUM4HSIEOO7vJC8Mwa2FkHURZBDSM2KAWu4CwPP5LEgtyZMCy/OzIgKBgGA2BUAGliPDxxreldFM2EU4JAOgwRtSPeHdmj0/Z3z8fcEQasTCiATgEgxMURvNAkR2sOIA2MtBSH60KNbC4dAhAHhBACFDdrBiADj2A0DI7PA4AgB0dihWAaBBXUk92Hhkdqi+BsDGh8ySzg7tGgD9BgC88FrgvgKQIeY/ZgepxXB3m6EFm6bm7ADqQwDggSLUHQVA2KG6P50JQJ+a96UBuC08OwqRerOWGPftkUa7Oc/GrTBKo5b0mEivwzOLAlABjb8v6EY1CXVGgwCAampUqZ5NNP5fuS1aB6w87Zn1PG+XpStOKw3AIro5IONfM0e8EhRsZFxz7EYRACRmgQc0YPMAENrAdJk6UBgAKwojz/W/yQUgXgilARguS5dUd2Yir+foPzSQ7TAfifdLOjZvzhSzQID0A3gmuJTDzIQzRXg4IC9IvFsY68jzWxr/qgvQV2TsRJ1gwMXOIuW+JNWhjtx2DWCDE6fvAOhNUQWgABSAAlAACkABKAAFoABOp1+6Bd0LJ+BorgAAAABJRU5ErkJggg=="
-                alt="mostly sunny"
-              />
-              <strong> 4 </strong>
-              <span className="units"> °C</span>
+  if (weatherData.loaded) {
+    return (
+      <div className="Weather">
+        <div className="Search">
+          <form>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  className="form-control"
+                  autoFocus="on"
+                  placeholder="Enter a city"
+                />
+              </div>
+              <div className="col-3 ">
+                <input
+                  type="submit"
+                  className="btn btn-primary w-100"
+                  value="Search"
+                />
+              </div>
             </div>
-          </div>
+          </form>
+        </div>
 
-          <div className="col-6">
-            <ul>
-              <li>Humidity: 44 %</li>
-              <li>Wind: 3 km/h</li>
-            </ul>
+        <div className="Overview">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
+          </ul>
+        </div>
+
+        <div>
+          <div className="row mt-3">
+            <div className="col-6">
+              <div className="d-flex weather-temperature">
+                <img
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAjdJREFUeNrtmsGtgzAMhjtCRmAERsgIHYFjjx2BERiBEToCI+TaG2+DbJBnKj8pD1Eaiv8AwpX+C1Ap/hzHjpNLCOFyZl0UgAJQAApAASgABaAAFIACODEA9C/83AypIrUkRwpv5Pib4dsCNp5cAMgIS3rMGPxJw3/t4QDQoEtSt8LwsTpJEFAAAONjNUM4HSIEOO7vJC8Mwa2FkHURZBDSM2KAWu4CwPP5LEgtyZMCy/OzIgKBgGA2BUAGliPDxxreldFM2EU4JAOgwRtSPeHdmj0/Z3z8fcEQasTCiATgEgxMURvNAkR2sOIA2MtBSH60KNbC4dAhAHhBACFDdrBiADj2A0DI7PA4AgB0dihWAaBBXUk92Hhkdqi+BsDGh8ySzg7tGgD9BgC88FrgvgKQIeY/ZgepxXB3m6EFm6bm7ADqQwDggSLUHQVA2KG6P50JQJ+a96UBuC08OwqRerOWGPftkUa7Oc/GrTBKo5b0mEivwzOLAlABjb8v6EY1CXVGgwCAampUqZ5NNP5fuS1aB6w87Zn1PG+XpStOKw3AIro5IONfM0e8EhRsZFxz7EYRACRmgQc0YPMAENrAdJk6UBgAKwojz/W/yQUgXgilARguS5dUd2Yir+foPzSQ7TAfifdLOjZvzhSzQID0A3gmuJTDzIQzRXg4IC9IvFsY68jzWxr/qgvQV2TsRJ1gwMXOIuW+JNWhjtx2DWCDE6fvAOhNUQWgABSAAlAACkABKAAFoABOp1+6Bd0LJ+BorgAAAABJRU5ErkJggg=="
+                  alt={weatherData.description}
+                />
+                <strong> {Math.round(weatherData.temperature)} </strong>
+                <span className="units"> °C</span>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity} %</li>
+                <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=e0a5a97de9a0b7a951e9d154a8f9bad8&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+
+    return "Loading...";
+  }
 }
