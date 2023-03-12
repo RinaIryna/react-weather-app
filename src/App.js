f9b3274161c7f689f5b784d4b8e965f20470ef66
RinaIryna <soulfelis@gmail.com>
2023-03-12 16:11:10 +0200

    Added bootstrap and created html "Weather"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <Weather />
+      </div>
     </div>
   );
 }
